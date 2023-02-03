02e0ff8f63390731167938331d4a74d1c0105ca8
Jesse P <jesseorhs@gmail.com>
2023-02-02 16:33:54 -0800

    Add mouseover event to grid, css styling to fill boxes on mouseover

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const container = document.querySelector(".container");
 
-
-
 for (let i=1; i < 17; i++){
     const row = document.createElement("div");
     row.id = "row" + i;
@@ -11,9 +9,15 @@ for (let i=1; i < 17; i++){
     for (let j=1; j < 17; j++){
         const column = document.createElement("div");
         column.id= "column" + j;
-        column.textContent = `test`
         column.classList.add("column");
         row.appendChild(column);
     }
 }
 
+const boxListen = document.querySelectorAll('.column')
+
+for (let i = 0; i < boxListen.length; i++){
+    boxListen[i].addEventListener("mouseover", function(){
+        boxListen[i].classList.add("fill")
+    })
+}
