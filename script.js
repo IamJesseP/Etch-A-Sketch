105451fb459d0d3b7be7d8aea0759a33de77d5c6
Jesse P <jesseorhs@gmail.com>
2023-02-02 20:06:33 -0800

    Organize code into functions, add shake animation, update styling

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
 
-const container = document.querySelector(".container");
+function removes(){
+    red.classList.remove('animation');
+    
+}
 
-
-
-
-
-
-for (let i=1; i < 16; i++){
-    const row = document.createElement("div");
-    row.id = "row" + i;
-    row.classList.add("row")
-    container.appendChild(row);
-
-    for (let j=1; j < 30; j++){
-        const column = document.createElement("div");
-        column.id= "column" + j;
-        column.classList.add("column");
-        row.appendChild(column);
+function clearSketch(){
+    for (let i = 0; i < boxListen.length; i++){
+        boxListen[i].classList.remove('fill');
     }
 }
 
-const boxListen = document.querySelectorAll('.column')
+function initializeSketch(){
+    for (let i=1; i < 16; i++){
+        const row = document.createElement("div");
+        row.id = "row" + i;
+        row.classList.add("row")
+        container.appendChild(row);
+        
+        for (let j=1; j < 30; j++){
+            const column = document.createElement("div");
+            column.id= "column" + j;
+            column.classList.add("column");
+            row.appendChild(column);
+        }
+    }
+}
+const container = document.querySelector(".container");
+initializeSketch();
+
+const red = document.querySelector(".red")
+const boxListen = document.querySelectorAll('.column');
+
 
 for (let i = 0; i < boxListen.length; i++){
     boxListen[i].addEventListener("mouseover", function(){
-        boxListen[i].classList.add("fill")
+        boxListen[i].classList.add(`fill`);
     })
 }
+
+const shake = document.querySelector(".shake");
+
+shake.addEventListener("click", ()=>{
+    red.classList.add('animation');
+    setTimeout(clearSketch, 700)
+    setTimeout(removes, 1000)
+})
+
